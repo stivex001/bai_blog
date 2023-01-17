2e4f2a44cf2b5dbc3b3d40adc77e55e7bf264ca2
stivex001 <stephenadeyemo@gmail.com>
2023-01-17 16:41:23 +0100

    implemented sorting features using query parameter

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,38 +1,45 @@
-import React from 'react'
-import { Route, useParams } from 'react-router-dom'
-import Comments from '../components/comments/Comments'
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import React from "react";
+import { Link, Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const Quotes_data = [
-    {
-      id: 'q1',
-      author: "Stephen",
-      text: "Consistency not speed",
-    },
-    {
-      id: 'q2',
-      author: "Oladunjoye",
-      text: "HardWork Pays",
-    },
-  ];
+  {
+    id: "q1",
+    author: "Stephen",
+    text: "Consistency not speed",
+  },
+  {
+    id: "q2",
+    author: "Oladunjoye",
+    text: "HardWork Pays",
+  },
+];
 
 const QuoteDetail = () => {
-    const params = useParams()
+  const params = useParams();
 
-    const quote = Quotes_data.find(quote => quote.id === params.quoteId)
+  const quote = Quotes_data.find((quote) => quote.id === params.quoteId);
 
-    if (!quote) {
-        return <p>oops! Quotes not found</p>
-    }
+  if (!quote) {
+    return <p>oops! Quotes not found</p>;
+  }
 
   return (
     <div>
-        <HighlightedQuote text={quote.text} author={quote.author} />
-        <Route path={`/quotes/${params.quoteId}/comments`} >
-            <Comments />
-        </Route>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </div>
-  )
-}
+  );
+};
 
-export default QuoteDetail
+export default QuoteDetail;
