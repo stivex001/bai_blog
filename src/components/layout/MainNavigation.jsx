eb6e9a1e95b993bee0be3702203a7d7aa4a5de3a
stivex001 <stephenadeyemo@gmail.com>
2023-01-17 13:07:38 +0100

    added react-router package to the project

---
+++ src/components/layout/MainNavigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import classes from './MainNavigation.module.css'
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+        <div className={classes.logo}>Great Quotes</div>
+        <nav className={classes.nav}>
+            <ul>
+                <li><NavLink to='/quotes'>All Quotes</NavLink></li>
+                <li><NavLink to='/newQuote'>All Quotes</NavLink></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default MainNavigation
