b36adf1ddd0c8763bbcfabfe6b27f3cf86544a4a
stivex001 <stephenadeyemo@gmail.com>
2023-06-05 14:21:44 +0100

    added stylings to the footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>
-      <div>2023 all right reserved</div>
-      <div></div>
+    <div className={styles.container}>
+      <div>&#169;2023 all right reserved</div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} alt="facebook" className={styles.icon}/>
+        <Image src="/2.png" width={15} height={15} alt="instagram" className={styles.icon}/>
+        <Image src="/3.png" width={15} height={15} alt="twitter" className={styles.icon}/>
+        <Image src="/4.png" width={15} height={15} alt="youtube" className={styles.icon}/>
+      </div>
     </div>
   );
 };
