ee685749d40c816fffbe94eea83f3e28249c7a9c
stivex001 <stephenadeyemo@gmail.com>
2023-06-05 23:46:12 +0100

    fix: code refactored

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import styles from "./category.module.css"
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const Category = ({ params }) => {
+  return <div className={styles.container}>
+    <h1>{params.category}</h1>
+  </div>;
+};
 
-export default page
+export default Category;
