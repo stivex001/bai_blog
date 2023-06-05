d97a9341e300d2ee578210a5bbc85bbf7fcd8025
stivex001 <stephenadeyemo@gmail.com>
2023-06-05 17:02:27 +0100

    feat: implemented contact us page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import styles from './page.module.css'
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const Contact = () => {
   return (
-    <div className={styles.contact}>Contact</div>
-  )
-}
+    <div className={styles.contact}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src="/contact.png" alt="" fill={true} />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="email" placeholder="email" className={styles.input} />
+          <textarea  cols="30" rows="10" className={styles.textArea}></textarea>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
