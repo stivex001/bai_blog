04d9d0417c539f0081519742e46c44314e17565d
stivex001 <stephenadeyemo@gmail.com>
2023-06-05 14:53:51 +0100

    feat: implemented the hero page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,22 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "public/hero .png";
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products,
+        </h1>
+        <p className={styles.desc}>
+          Turning your idea into reality. we bring together global tech industry
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
     </div>
-  )
+  );
 }
