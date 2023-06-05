c9caead3baafed59b787f81bb4196beff781ed7e
stivex001 <stephenadeyemo@gmail.com>
2023-06-05 23:11:11 +0100

    feat: implemented portfolio component page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations">
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites">
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/application">
+          <span className={styles.title}>Applications</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
