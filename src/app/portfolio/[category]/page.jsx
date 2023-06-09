a58fd2e76c9a5304330e0b271136bfb09fc9bbbe
stivex001 <stephenadeyemo@gmail.com>
2023-06-09 13:27:14 +0100

    added stylings to category page

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,11 +1,59 @@
 import React from "react";
-import styles from "./category.module.css"
+import styles from "./category.module.css";
+import Button from "../../../components/Button/Button";
+import Image from "next/image";
 
 const Category = ({ params }) => {
-  return <div className={styles.container}>
-    <h1 className={styles.catTitle}>{params.category}</h1>
-    
-  </div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/6146812/pexels-photo-6146812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/6146812/pexels-photo-6146812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/6146812/pexels-photo-6146812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className={styles.img}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
