95a406a611dae48205adef0776b366b3059bbac0
stivex001 <stephenadeyemo@gmail.com>
2023-06-10 14:47:54 +0100

    added terraform configuration

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws= {
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "tcb-app-qa-jr5"
+}
+
