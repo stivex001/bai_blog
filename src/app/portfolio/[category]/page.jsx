a0e7a72fd1bb215ab1ecaedf5496973f52ab7cee
stivex001 <stephenadeyemo@gmail.com>
2023-06-12 19:51:28 +0100

    feat:code refactored

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -19,21 +19,23 @@ const Category = ({ params }) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See More" url="#" />
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title} </h1>
+            <p className={styles.desc}>{item.desc} </p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              fill={true}
+              src={item.image}
+              alt=""
+              className={styles.img}
+            />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image
-            fill={true}
-            src="https://images.pexels.com/photos/6146812/pexels-photo-6146812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            className={styles.img}
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
