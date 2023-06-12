61e3e83f7e71d8fca0373e00360b969148d10d31
stivex001 <stephenadeyemo@gmail.com>
2023-06-12 16:35:51 +0100

    feat:added useSwr package for data fetching

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,22 +1,15 @@
+"use client"
+
 import React, { useEffect, useState } from "react";
+import useSWR from 'swr'
 
 const Dashboard = () => {
-  const [data, setData] = useState();
+  
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  
+  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
 
-  useEffect(() => {
-    const getData = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-        cache: "no-store",
-      });
-
-      if (!res.ok) {
-        throw new Error("Failed to fetch data");
-      }
-
-      return res.json();
-    };
-  }, []);
-
+  console.log(data);
   return <div>Dashboard</div>;
 };
 
